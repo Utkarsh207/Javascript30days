56a9b6c79afed6cd97de013cd4cbe6a220f35080
Utkarsh207 <153854083+Utkarsh207@users.noreply.github.com>
2024-07-16 07:40:38 +0000

    Learnt difference between var and let and different data types

---
+++ Day1/VariableDataType.js
@@ -0,0 +1,71 @@
+let num1 = 12;
+// console.log(typeof num1);
+// console.log(num1);
+
+let str = 'John cena';
+// console.log(typeof str);
+// console.log(str);
+
+let Bool = true;
+// console.log(typeof Bool);
+// console.log(Bool);
+
+
+let arr = [1,2,3,4];
+// console.log(typeof Bool);
+// console.log(Bool);
+
+let obj = {
+    name : 'Undertaker',
+    age : 59,
+    heigth : '6feet 8 inches',
+
+};
+// console.log(obj);
+// console.log(typeof obj);
+
+
+
+
+
+// Part 2
+let a = 12;
+// console.log(a);
+a = 34;
+// console.log(a);
+
+// Using let you can reassign the value
+
+
+const author = 'CM PUNK';
+// console.log(author);
+// author = 'JOHN CENA';
+// console.log(author);
+
+// Using const you can reassign the value 
+
+
+
+var num2= 12;
+{
+    num2 =14
+}
+// console.log(num2);
+
+// Var does not have block scope 
+// So it is not used
+
+// {
+//     var x = 12 
+// }
+// // x = 15;
+// console.log(x);
+
+{
+    let z = 15;
+}
+
+// console.log(z);
+
+
+console.log('Utkarsh');
