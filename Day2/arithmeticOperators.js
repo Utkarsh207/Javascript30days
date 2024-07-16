99fa0d777e430062db06143f5ce653c79ee4332e
Utkarsh207 <153854083+Utkarsh207@users.noreply.github.com>
2024-07-16 08:39:46 +0000

    Executed different operators

---
+++ Day2/arithmeticOperators.js
@@ -0,0 +1,63 @@
+// Add two numbers
+let num1 = 10;
+let num2 = 20;
+// console.log(num1+num2);
+
+// Subtract  two numbers
+let num3 = 30;
+let num4 = 20;
+// console.log(num3-num4);
+
+// Multiply two numbers
+// console.log(num3*num4);
+// Divide two numbers
+// console.log(num3/num4);
+
+// Finding remainder
+// console.log(num3%num4);
+
+// Assignment Operators
+let sum = 10;
+sum+= 10;
+// console.log(sum);
+sum-=10;
+// console.log(sum);
+
+let a = 20;
+let b = 20;
+// console.log(a>b); // false
+// console.log(a<b); // false
+// console.log(a>=b); // True
+// console.log(a<=b); // True
+
+let str1 = '12';
+let str2 = 12;
+// console.log(str1==str2);
+//console.log(str1===str2); //Strict equality test test the data type also
+
+//  Logical Operators
+// let age = 5;
+// if(age<18 || age>60){ // 5>=18 && 
+//     console.log('You are not eligible to drive');
+// }
+// else{
+//     console.log('You are eligible to drive');
+// }
+// let age = 21;
+// let addharCard = false;
+// if(age >18 && addharCard){
+//     console.log('You are eligible to drive');
+// }else{
+//     console.log('You are not eligible to drive');
+// }
+
+// let pan = false;
+// if(!pan){
+//     console.log('Please Take your pan card first');
+// }else{
+//     console.log('Eligible for futher process');
+// }
+
+// Ternary Operator
+let number = -1;
+number>0? console.log('Postive number') : console.log('Negative Number');
