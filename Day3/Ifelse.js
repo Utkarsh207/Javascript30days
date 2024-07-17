e13ff24c4e5c616989e45f65445d6aafe5b9266f
Utkarsh207 <153854083+Utkarsh207@users.noreply.github.com>
2024-07-17 09:04:21 +0000

    Learnt ifElse&& ternary operator

---
+++ Day3/Ifelse.js
@@ -0,0 +1,116 @@
+// To check number is positive , negative or zero
+// let num = 0;
+// if(num>0){
+//     console.log('Positive Number');
+// }
+// else if(num<0){
+//     console.log('Negative Number');
+// }
+// else{
+//     console.log(`Number is ${num}`);
+// }
+
+
+// Eligiblity Test for voting
+// let num =  5;
+// if(num>=18){
+//     console.log("Eligible for voting");
+// }
+// else{
+//     console.log("Not eligible for voting"); 
+// }
+
+// To find largest of three
+let a = 5;
+let b = 10;
+let c = 8;
+let largest;
+
+// if(num1>num2 && num1>num3){
+//     console.log(`${num1} is greatest`);
+// }
+// else if(num2>num1 && num2>num3){
+//     console.log(`${num2} is greatest`);
+// }
+// else{
+//     console.log(`${num3} is greatest`);
+// }
+
+// if(true){
+//     if(a>b && a>c){
+//         largest = a;
+//     }
+//     else if(b>a && b>c){
+//         largest = b;
+//     }
+//     else{
+//         largest = c;
+//     }
+    
+// }
+// console.log('C : '+ largest);
+
+// Switch Case
+// let day = 13;
+// switch(day){
+//     case 1 :
+//         console.log('Monday');
+//         break;
+//     case 2 :
+//         console.log('Tuesday');
+//         break;
+//     case 3 :
+//         console.log('Wednesday');
+//         break;
+//     case 4 :
+//         console.log('Thursday');
+//         break;
+//     case 5 :
+//         console.log('Friday');
+//         break;
+//     case 6 :
+//         console.log('Saturday');
+//         break;
+//     case 7 :
+//         console.log('Sunday');
+//         break;
+//     default:
+//         console.log("Default case");
+//         break; 
+// }
+
+// let grade = 'f';
+// switch(grade){
+//     case 'A':
+//         console.log('A grade');
+//         break;
+//     case 'B':
+//         console.log('B grade');
+//         break;
+//     case 'C':
+//         console.log('C grade');
+//         break;
+//     case 'D':
+//         console.log('D grade');
+//         break;
+//     case 'E':
+//         console.log('E grade');
+//         break;
+//     default :
+//         console.log('No worries you will comeback stronger');
+//         break;
+// }
+
+ // Ternary operator
+// let num1 = 13;
+// num1%2==0 ? console.log("Even Number") : console.log("Odd Number");
+
+
+//  Leap Year
+let year = 1800;
+if((year%4==0 &&year%100!=0) || year%400==0){
+    console.log('Is a Leap Year');
+}else{
+    console.log('Not a leap year');
+}
+
