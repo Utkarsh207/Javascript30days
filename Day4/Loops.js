2daf2e3684709a1642dcde0a393da0bc765ac1cd
Utkarsh207 <153854083+Utkarsh207@users.noreply.github.com>
2024-07-19 07:34:44 +0000

    Loops

---
+++ Day4/Loops.js
@@ -0,0 +1,66 @@
+// Print numbers from 1 to 10
+
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+// Table of 5
+
+// for(let i=1; i<=12; i++){
+//     let num = 5;
+//     num*=i;
+//     console.log(num);
+// }
+
+// Sum of numbers from 1 to 10
+// let sum = 0;
+// let i = 1;
+// while(i<=10){
+//     sum+=i;
+//     i++;
+// }
+// console.log(sum);
+
+// Printing 10 to 1
+
+// let i =10;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+
+// Printing 1 to 5 using Do while loop
+// let num = 1;
+// do{
+//     console.log(num);
+//     num++;
+// }while(num<=5);
+
+// Factorial Using do while loop
+// let fact = 1;
+// let i =1;
+// do{
+//     fact*=i;
+//     i++;
+// }while(i<=3);
+// console.log(fact);
+
+// Triangle Pattern 
+// for(let i=1; i<=5; i++){
+//     let row =""
+//     for(let j=1; j<=i; j++){
+//         row+="*";
+//     }
+//     console.log(row);
+    
+// }
+// Continue Statement
+// for(let i=1; i<=10; i++){
+//     if(i==5) continue;
+//     console.log(i);
+// }
+// Break Statement
+for(let i=1; i<=10; i++){
+    if(i==7) break;
+    console.log(i);
+}
