47b21d676cfbd787f9ba1ab64c74090208f57cfa
Utkarsh207 <153854083+Utkarsh207@users.noreply.github.com>
2024-07-20 08:09:39 +0000

    Arrays

---
+++ Day6/Array.js
@@ -0,0 +1,57 @@
+// Creating a array
+// let arr = [1,2,3,4,5];
+// console.log(arr);
+
+// Accessing array elements
+// let arr = [1,2,3,4,5];
+// console.log(arr[0]);
+// console.log(arr[4]);
+
+// Basic Method of arr
+// let arr = [1,2,3,4,5];
+// arr.push(10);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+// Shift And Unshift
+// let arr = [1,2,3,4,5];
+// arr.unshift(3);
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+
+// Map
+// let arr = [1,2,3,4,5];
+// let newArr = arr.map(num=>{
+//     return num*2;
+// })
+// console.log(newArr);
+
+// filter
+// let arr = [100,2,3,43,2,5,42,54,554,45,4];
+// let evenArr = arr.filter((num,indx)=>{
+//     if(num%2==0) return true;
+//     else return false;
+// })
+// console.log(evenArr);
+// let evenArr = arr.filter((num,indx)=>num%2==0);
+// console.log(evenArr);
+
+
+// Reduce Method
+
+
+
+
+// Array iteration
+// for loop
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// for(let i =0; i<arr.length;i++){
+//     console.log(arr[i]);
+
+// Using forEach
+let arr = [1,2,3,4,5,6,7,8,9,10];
+ arr.forEach((num,indx)=>{
+    console.log(num);
+})
