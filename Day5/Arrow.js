a3866feffc437df579eb5bec4cd04acaf389b671
Utkarsh207 <153854083+Utkarsh207@users.noreply.github.com>
2024-07-20 06:57:34 +0000

    Added HOF

--- Day5/Arrow.js
+++ Day5/Arrow.js
@@ -20,8 +20,41 @@ let add =(num1,num2)=>num1+num2;
 // console.log(p(4,3));
 
 
-let greetings = (name,age =18)=>{
-    console.log(`Hello ${name} have good day`);
+// let greetings = (name,age =18)=>{
+//     console.log(`Hello ${name} have good day`);
+// }
+
+// greetings('Utkarsh',20)
+
+// Higher Order Functions - A function which takes another function as a arugment or returns a function from it is known as higher order function.
+
+// Task1
+// function x(){
+//     return 'You are inside the function x';
+// }
+// function print(func1,num){
+//     for(let i=0; i<num; i++){
+//         console.log(x());
+//     }
+// }
+// print(x,5)
+
+// Task2
+// function greet(){
+//     return 'Hello'
+// }
+// function name(){
+//     return 'Utkarsh';
+// }
+
+function num1(num){
+    return num+1;
+}
+function num2(num){
+    return num+2;
 }
 
-greetings('Utkarsh',20)
+function higerOrderfun(func1,func2,value){
+    console.log(func2(func1(value)));
+}
+higerOrderfun(num1,num2,5)
