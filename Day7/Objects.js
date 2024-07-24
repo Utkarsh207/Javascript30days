1afeebb37ef3dbef01eb5205d42334734825d310
Utkarsh207 <153854083+Utkarsh207@users.noreply.github.com>
2024-07-24 06:54:22 +0000

    Objects

---
+++ Day7/Objects.js
@@ -0,0 +1,50 @@
+// Task 1
+// let books = {
+//     title : 'Legends',
+//     Author : 'Utkarsh',
+//     year : '2003',
+//     // 'date-of-publication' : '2003',
+// };
+// Task 2
+// console.log(books);
+// console.log(books.Author);
+// console.log(books.year);
+// console.log(books.title);
+// console.log(books['year']);
+// console.log(books.Author);
+// console.log(books["date-of-publication"]);
+
+
+// Task 3
+// let books = {
+//     title : 'Legends',
+//     Author : 'Utkarsh',
+//     year : '2003',
+
+//     displayInfo(){
+//         return `Title : ${this.title} , Author : ${this.Author}`
+//     },
+
+//     updatedYear(year){
+//         this.year = year;
+//     }
+        
+//     };
+//     console.log(books.displayInfo());
+//     // console.log(books.updatedYear(2006));
+//     console.log(books.year);
+
+
+// Task 3 
+let library = {
+    name : 'Best Library of the world',
+    books : [
+        {
+            title : "Book1",
+            Author : "Author1",
+        }
+    ]
+
+
+};
+console.log(library);
