0c1965cd201356eaacb9614381eff4c6cd72bf74
Utkarsh207 <153854083+Utkarsh207@users.noreply.github.com>
2024-07-31 16:57:47 +0000

    Selecting & Manipulating using DOM

---
+++ Day9/DOM.js
@@ -0,0 +1,8 @@
+// Task 1: Select an HTML element by its ID and change its text content.
+// let h = document.getElementById('heading1');
+// h.textContent = 'Changed Heading';
+
+// Task 2: Select an HTML element by its class and change its background color.
+let h = document.getElementsByClassName('heading1');
+h.style.backgroundColor = 'black';
+h.style.color = 'yellow';
