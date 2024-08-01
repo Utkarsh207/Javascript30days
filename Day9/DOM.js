259efbe24cc3ddeb57f7d3a7e00e6d3e8e89004b
Utkarsh207 <153854083+Utkarsh207@users.noreply.github.com>
2024-08-01 08:35:00 +0000

    DOM manipulation

--- Day9/DOM.js
+++ Day9/DOM.js
@@ -3,6 +3,57 @@
 // h.textContent = 'Changed Heading';
 
 // Task 2: Select an HTML element by its class and change its background color.
-let h = document.getElementsByClassName('heading1');
-h.style.backgroundColor = 'black';
-h.style.color = 'yellow';
+// let h = document.getElementsByClassName('heading1');
+// h.style.backgroundColor = 'black';
+// h.style.color = 'yellow';
+
+// <!-- TASK 3 -->
+// let newDiv = document.createElement('div');
+// newDiv.textContent = 'This Is a new Div';
+// document.body.append(newDiv);
+
+// let newPara = document.createElement('p');
+// newPara.textContent = 'Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc.'
+// document.body.append(newPara);
+
+ // <!-- TASK 4 -->
+// let list = document.createElement('li');
+// list.textContent = 'Golang';
+// document.querySelector('ul').appendChild(list);
+
+// <!-- TASK 5 -->
+// newPara.remove();
+// let removedDiv = document.querySelector('#div');
+// removedDiv.remove();
+
+// <!-- TASK 6 -->
+// let removedChild = document.querySelector('ul');
+// removedChild.lastElementChild.remove();
+// removedChild.lastElementChild.remove();
+
+// <!-- TASK 7 -->
+document.querySelector('img').src = "https://parade.com/.image/t_share/MTkzMDEwNDIzOTkyMTY1NTk3/inspirational-quote.png";
+// <!-- TASK 8 -->
+// <!-- TASK 9 -->
+let flag = 0;
+let para = document.querySelector('#para');
+let button = document.querySelector('button');
+button.addEventListener('click', function(){
+    if(flag ==0){
+    para.textContent = 'Never Fold Never Backdown - SMW';
+    flag = 1;
+    }
+    else{
+        para.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, delectus?';
+        flag =0;
+    }
+})
+// <!-- TASK 10 -->
+let border = document.querySelector('#border');
+border.addEventListener('mouseover', function(){
+    border.style.border = '2px solid crimson'
+})
+
+
+
+
