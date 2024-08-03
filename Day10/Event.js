b35de800c0e00de9b75d1adbe85d15ecc6672c2c
Utkarsh207 <153854083+Utkarsh207@users.noreply.github.com>
2024-08-03 09:05:25 +0000

    Event Handling

---
+++ Day10/Event.js
@@ -0,0 +1,48 @@
+// <!-- TASK 1 -->
+// let p = document.querySelector('p');
+// let button = document.querySelector('button');
+// let flag = 0; 
+// button.addEventListener('click',function(){
+//     if(flag==0){
+//         p.textContent = 'Never Fold Never Backdown - SMW';
+//         flag = 1;
+//     }
+//     else{
+//         p.textContent = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, tenetur.';
+//         flag = 0;
+//     }
+// })
+// <!-- TASK 2 -->
+// let button = document.querySelector('button');
+// let img = document.querySelector('img');
+// button.addEventListener('dblclick', function(){
+//     if(img.style.display ==='none'){
+//         img.style.display = 'block';
+//     }
+//     else{
+//         img.style.display = 'none';
+//     }
+// })
+
+// <!-- TASK 3 -->
+// let button = document.querySelector('button');
+// let flag = 0;
+// button.addEventListener('mouseover', function(){
+//    if(flag ==0){
+//     button.style.backgroundColor = 'royalBlue';
+//     button.style.color = 'salmon';
+//     flag = 1;
+//    }
+//    else{
+//     button.style.backgroundColor = 'black';
+//     button.style.color = 'yellow';
+//     flag = 0;
+//    }
+// })
+
+// <!-- TASK 4 -->
+let button = document.querySelector('button');
+button.addEventListener('mouseover', function(){
+    button.style.backgroundColor = 'black';
+    button.style.color = 'aliceblue';
+})
